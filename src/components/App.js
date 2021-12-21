63bd887e9b80925a0c755fa392cd24b5506da984
hwryeol <hwwy0425@gmail.com>
2021-12-21 14:13:57 +0900

    add file

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+
+import react from "react";
+import AppRouter from "./Router"
+function App() {
+  return <AppRouter />
+}
+
+export default App;
---
+++ src/routes/EditProfiles.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+
+const EditProfiles = () => <span>EditProfiles</span>
+export default EditProfiles
